12f34913f8b1760aac0f6da4641d01939f2e4996
jqzhu <zhujiaqiang>
2018-03-29 09:25:22 +0800

    Spring Boot干货系列（四）：开发Web应用之Thymeleaf篇

---
+++ demo04-web-thymeleaf/src/main/java/com/jqzhu/Demo04WebThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.jqzhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo04WebThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo04WebThymeleafApplication.class, args);
+	}
+}
